6d9d1e2a4ecc792149484b4406fe91846b23c732
serim_Kim <50200959+serim01@users.noreply.github.com>
2024-05-03 09:34:13 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,15 +3,11 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        ArrayList<Integer> intList = new ArrayList<>();
         while(n>0){
-            intList.add(n % 10);
+            answer += n % 10;
 	        n /= 10;
         }
-        
-        for(int i=0; i<intList.size(); i++){
-                answer += intList.get(i);
-        }
+
         return answer;
     }
 }
