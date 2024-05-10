83cb64c5a7b4997077560ad533db2772b24b583d
serim_Kim <50200959+serim01@users.noreply.github.com>
2024-05-10 09:37:28 +0900

    [level 1] Title: 하샤드 수, Time: 0.14 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String[] list = Integer.toString(x).split("");
+        int sum = 0;
+        for (String s : list) {
+            sum += Integer.parseInt(s);
+        }
+        if (x%sum == 0){
+            answer = true;
+        }else{
+            answer = false;
+        }
+        return answer;
+    }
+}
