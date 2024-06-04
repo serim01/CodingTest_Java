e8bfbf93038691325d9d9083923b3f65a851e2fa
serim_Kim <50200959+serim01@users.noreply.github.com>
2024-06-04 09:12:00 +0900

    [level 1] Title: 최소직사각형, Time: 1.30 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max_row = 0;
+        int max_col = 0; 
+        
+        for(int i=0;i<sizes.length;i++){
+            if(sizes[i][0]<sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+            if(max_row<sizes[i][0]) max_row = sizes[i][0]; 
+            if(max_col<sizes[i][1]) max_col = sizes[i][1]; 
+        }
+        answer = max_row * max_col;
+        return answer;
+    }
+}
